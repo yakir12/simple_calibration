ae46d35c461c8e6f63e6c431d5f9faef456695fb
Yakir Gagnon <12.yakir@gmail.com>
2021-11-04 22:22:37 +0100

    tilde!

--- one.jl
+++ one.jl
@@ -17,7 +17,7 @@ itform = PerspectiveMap() ∘ H ∘ push1 # this is the inverse transform
 tform =  PerspectiveMap() ∘ inv(H) ∘ push1 # this is the transform
 @assert all(isapprox.(tform.(tag.p), corners, atol = 1)) # test by transforming the tag's corners from pixel coordinates to world coordinates
 @assert all(isapprox.(itform.(corners), tag.p, atol = 1)) # and test by inverse transforming the world coordinates of the corners to image coordinates
-imgw = warp(img, itform, ImageTransformations.autorange(img, tform)) # warp the image
+imgw = warp(img', itform, ImageTransformations.autorange(img, tform)) # warp the image
 imshow(imgw) # first obvious problem is the scaling
 
 
@@ -30,7 +30,7 @@ itform = PerspectiveMap() ∘ H ∘ push1 ∘ iscale # add the scaling to the tr
 tform =  scale ∘ PerspectiveMap() ∘ inv(H) ∘ push1
 @assert all(isapprox.(tform.(tag.p), corners, atol = 1)) # testing worked
 @assert all(isapprox.(itform.(corners), tag.p, atol = 1))
-imgw = warp(img, itform, ImageTransformations.autorange(img, tform)) # nice, the image size more useful
+imgw = warp(img', itform, ImageTransformations.autorange(img, tform)) # nice, the image size more useful
 imshow(imgw) # but the rectification is wrong!?
 
 
